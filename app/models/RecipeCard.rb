4a28ae4d0605ba0a427de99654b6f496d7fb0289
cannoli9917 <katie.pennachio@gmail.com>
2019-01-15 15:13:05 -0500

    mini project with Eli

---
+++ app/models/RecipeCard.rb
@@ -0,0 +1,28 @@
+class RecipeCard
+
+  attr_reader :user, :date, :recipe
+  attr_accessor :rating
+
+  @@all = []
+
+  def initialize(user, date, rating, recipe)
+    @date = date
+    @user = user
+    @rating = rating
+    @recipe = recipe
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+
+
+
+
+end
