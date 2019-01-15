7ee7dcd61afa1c8f1f11d942f5ad8fa904a12e8b
mistermur <brian.j.murillo@gmail.com>
2019-01-15 14:21:20 -0500

    done

---
+++ app/models/User.rb
@@ -0,0 +1,57 @@
+
+
+class User
+  attr_reader :name
+  @@all =[]
+
+  def initialize(name)
+    @name = name
+    # @recipe = recipe
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def recipes
+    recipe_cards.map do |rc|
+      rc.recipe
+    end
+  end
+  def recipe_cards
+    RecipeCard.all.select do |rc|
+      rc.user == self
+    end
+
+  end
+  def declare_allergen(ingredient)
+    Allergen.new(self,ingredient)
+  end
+
+  def allergens
+    Allergen.all.select do  |a|
+      a.user == self
+    end
+  end
+
+  def add_recipe_card(recipe,date,rating)
+    RecipeCard.new(self,recipe,date,rating)
+  end
+
+  def recipe_sorter
+    recipe_cards.sort_by do |rc|
+       - rc.rating
+       # binding.pry
+     end
+     # binding.p
+  end
+
+  def top_three_recipes
+    recipe_sorter[0..2]
+  end
+  def most_recent
+    recipes[-1]
+  end
+
+end #end of class
